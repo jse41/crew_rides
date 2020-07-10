4a5b1110e78606ba834b75de205f6a8c04969286
Jacob E <37668374+jacobeng3l@users.noreply.github.com>
2020-07-10 08:53:33 -0400

    Added first aws lambda call

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,16 +10,58 @@ import CarTable from './../Components/CarTable'
 class Home extends Component {
 
   componentDidMount() {
-    let ticket = this.props.location.search.substr(this.props.location.search.indexOf('=') + 1)
-    if (ticket.length > 5) {
+    let tickets = this.props.location.search.substr(this.props.location.search.indexOf('=') + 1)
+    if (tickets.length > 5) {
       console.log("We found the ticket!")
-      console.log(ticket)
+      console.log(tickets)
     }
     else {
       window.location.replace('https://login.case.edu/cas/login?service=http://10.0.0.61:3000')
     }
+    // fetch("https://ddpfv93bg7.execute-api.us-east-2.amazonaws.com/default/crew-hello")
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       console.log("Successful GET")
+    //       console.log(result)
+    //     },
+    //     // Note: it's important to handle errors here
+    //     // instead of a catch() block so that we don't swallow
+    //     // exceptions from actual bugs in components.
+    //     (error) => {
+    //       console.log(error)
+    //       console.log("ERROR!")
+    //     }
+    //   )
+      console.log(JSON.stringify("Hello!"))
+    fetch('https://ddpfv93bg7.execute-api.us-east-2.amazonaws.com/default/crew-hello', {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+        "Accept-Charset": "UTF-8;",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Request-Headers": "x-requested-with",
+        "Access-Control-Allow-Headers": "x-requested-with"
+      },
+      body: JSON.stringify({ticket: tickets,
+                            service: "http://10.0.0.61:3000"})
+    }).then(res => res.json())
+    .then(
+      (result) => {
+        console.log("Successful POST")
+        console.log(result)
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        console.log(error)
+        console.log("ERROR!")
+      }
+    )
   }
-  
+
   render() {
     return (
       <div className="App">
