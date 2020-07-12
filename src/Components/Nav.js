6d9f3030622ec3d846f5237d2cbc2807f4cca7af
Jacob Engelbrecht <jacob.engelbrecht99@gmail.com>
2020-07-12 19:53:01 -0400

    Name in navbar
    last before deployment

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import './Styling/Navba.css';
 
 /**
  * Generate the Nav Bar
  */
 class Navba extends React.Component {
-   render() {
-       return (
-           <Navbar variant="dark">
-               <Navbar.Brand href="#home"><div className="navImg"><img src="/CircularOar.png" alt="Logo"></img></div></Navbar.Brand>
-               <Nav className="mr-auto">
-                   <Nav.Link as={Link}  to="/">Home</Nav.Link>
-                   <Nav.Link as={Link}  to="/Rides">Rides</Nav.Link>
-                   <Nav.Link as={Link}  to="/About">About</Nav.Link>
-                   <Nav.Link as={Link}  to="/Other">Other</Nav.Link>
-               </Nav>
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "?"
+        };
+    }
 
-           </Navbar>
-       )
-   }
+    updateName = () => {
+        this.setState({ name: "!"});
+    };
+
+    render() {
+        return (
+            <Navbar variant="dark">
+                <Navbar.Brand href="#home"><div className="navImg"><img src="/CircularOar.png" alt="Logo"></img></div></Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/Rides">Rides</Nav.Link>
+                    <Nav.Link as={Link} to="/About">About</Nav.Link>
+                    <Nav.Link as={Link} to="/Other">Other</Nav.Link>
+                </Nav>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        <p>{this.props.name ? `Signed in as: ` : ""}</p>
+                        <p className="navClick" onClick={this.updateName}>{this.props.name ? `${this.props.name}${this.state.name}` : "Click to Log In"}</p>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+
+            </Navbar>
+        )
+    }
 }
 export default Navba;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -22,43 +22,45 @@ class Home extends Component {
     // Describe what is sent 
     let flight = {
       ticket: tickets,
-      service: "http://10.0.0.61:3000",
-      cooks: localStorage.getItem('cooks')
+      service: "http://10.0.0.63:3000",
+      cooks: sessionStorage.getItem('cooks')
     };
 
-    // If there is a ticket, query the API, otherwise, redirect 
-    if (tickets.length > 5) {
-      console.log("We found the ticket!")
-      console.log(tickets)
-      fetch('https://ddpfv93bg7.execute-api.us-east-2.amazonaws.com/default/crew-hello', {
-        mode: 'cors',
-        method: 'POST',
-        headers: {
-          "Content-type": "application/json",
-          "Accept-Charset": "UTF-8;",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Request-Headers": "x-requested-with",
-          "Access-Control-Allow-Headers": "x-requested-with"
-        },
-        body: JSON.stringify(flight)
-      }).then(res => res.json())
-        .then(
-          (result) => {
-            console.log("Successful POST")
-            console.log(result)
-            if (result.code)
-              this.setState({ name: result.name })
-          },
+    console.log(`The session storage is: ${sessionStorage.getItem('cooks')}`)
 
-          (error) => {
-            console.log(error)
-            console.log("ERROR!")
+    // If there is a ticket, query the API, otherwise, redirect 
+    //console.log(tickets)
+    fetch('https://ddpfv93bg7.execute-api.us-east-2.amazonaws.com/default/crew-hello', {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+        "Accept-Charset": "UTF-8;",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Request-Headers": "x-requested-with",
+        "Access-Control-Allow-Headers": "x-requested-with"
+      },
+      body: JSON.stringify(flight)
+    }).then(res => res.json())
+      .then(
+        (result) => {
+          console.log("Successful POST")
+          console.log(result)
+          if (result.cooks) {
+            sessionStorage.setItem('cooks', result.cooks)
           }
-        )
-    }
-    else {
-      window.location.replace('https://login.case.edu/cas/login?service=http://10.0.0.61:3000')
-    }
+          if (result.reload) {
+            window.location.replace('https://login.case.edu/cas/login?service=http://10.0.0.63:3000')
+          }
+          if (result.code)
+            this.setState({ name: result.name })
+        },
+
+        (error) => {
+          console.log(error)
+          console.log("ERROR!")
+        }
+      )
   }
 
   render() {
@@ -66,7 +68,7 @@ class Home extends Component {
       return (
         <div className="App">
           <Alert />
-          <NavBa />
+          <NavBa name={this.state.name}/>
           <h1>Welcome to the Crew RideSheet!</h1>
           <CarTable />
           <Footer />
@@ -77,7 +79,7 @@ class Home extends Component {
       return (
         <div className="App">
           <Alert />
-          <NavBa />
+          <NavBa name={this.state.name}/>
           <h1>Hi {this.state.name}! Welcome to the Crew RideSheet!</h1>
           <CarTable />
           <Footer />
